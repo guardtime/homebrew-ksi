a5c78576e3aea64b9ed72415c20f60fe96e1ce20
Jamie Steiner <jvsteiner@gmail.com>
2017-04-16 21:01:03 +0300

    a test for ksitool?

--- Formula/ksi-tool.rb
+++ Formula/ksi-tool.rb
@@ -16,24 +16,11 @@ class KsiTool < Formula
 
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    # system "cmake", ".", *std_cmake_args
     system "./rebuild.sh", "--prefix=#{prefix}"
     system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test ksi-tool`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "ksi -h"
   end
 end
