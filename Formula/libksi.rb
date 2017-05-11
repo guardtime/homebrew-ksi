3dd3b07274e1b6df3afddcfd3f672119eb20ba67
Jamie Steiner <jvsteiner@gmail.com>
2017-05-11 21:33:39 +0200

    updated libksi version

--- Formula/libksi.rb
+++ Formula/libksi.rb
@@ -4,7 +4,7 @@
 class Libksi < Formula
   desc "C SDK for Keyless Signature Infrastructure (c) Guardtime"
   homepage "https://github.com/guardtime/libksi"
-  url "https://github.com/guardtime/libksi/archive/v3.12.2010.tar.gz"
+  url "https://github.com/guardtime/libksi/archive/v3.13.2043.tar.gz"
   sha256 "b6d0dff13e69a9ee12ef09701c0f50d275bcb1af54e4f465248a2e3fd361930e"
 
   depends_on "automake" => :build
