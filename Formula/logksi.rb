555f9eb8f33419c40899a8e9ab5f29ff580b44da
taavi.valjaots <taavi.valjaots@guardtime.com>
2018-02-16 13:21:29 +0200

    Logksi version changed from 1.1.258 to 1.2.323.

--- Formula/logksi.rb
+++ Formula/logksi.rb
@@ -1,8 +1,8 @@
 class Logksi < Formula
   desc "Log signature command-line tool for Keyless Signature Infrastructure (c) Guardtime"
   homepage "https://github.com/guardtime/logksi"
-  url "https://github.com/guardtime/logksi/archive/v1.1.258.tar.gz"
-  sha256 "767ade65eebb78e7875a81aa146770a364beff93925d5a50e9999325210e27bf"
+  url "https://github.com/guardtime/logksi/archive/v1.2.323.tar.gz"
+  sha256 "8fa5319c8e3731c6fc14b480b6c43f99a838a06fc181be0a7dbf020f2bf4899b"
 
   # Change revision if there is a need to recompile the formula 
   # and the version of the package is not changed. If version
